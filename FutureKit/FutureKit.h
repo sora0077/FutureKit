ede45d3a93f3fafb7ff9daf0536ff8e040c128f6
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-02-14 17:25:29 +0900

    Initial Commit

---
+++ FutureKit/FutureKit.h
@@ -0,0 +1,19 @@
+//
+//  FutureKit.h
+//  FutureKit
+//
+//  Created by 林達也 on 2015/02/14.
+//  Copyright (c) 2015年 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FutureKit.
+FOUNDATION_EXPORT double FutureKitVersionNumber;
+
+//! Project version string for FutureKit.
+FOUNDATION_EXPORT const unsigned char FutureKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FutureKit/PublicHeader.h>
+
+
